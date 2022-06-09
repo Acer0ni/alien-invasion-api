9e5a27cab09f270882705a662f76f62f58b0a1dd
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-09 10:45:21 -0700

    added users and highscore models

--- alien_invasion_api/settings.py
+++ alien_invasion_api/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "alien_invasion_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "alien_invasion",
+        "USER": "postgres",
+        "password": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
---
+++ api/models/highscore.py
@@ -0,0 +1,8 @@
+from django.db import models
+from models.users import User
+
+
+class Highscore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="highscore")
+    score = models.BigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
