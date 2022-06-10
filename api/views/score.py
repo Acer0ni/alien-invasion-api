65b21d2e191410bc43c6cb434b65ce6f2fbac895
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-10 13:48:04 -0700

    added functioning create scores route

--- api/views/score.py
+++ api/views/score.py
@@ -1,12 +1,35 @@
+from typing import List
 from ninja import Router
 from api.schemas.score import ScoreSchemaIn, ScoreSchemaOut
 from api.models.score import Score
+from api.models.users import User
 
 router = Router(tags=["score"])
 
-
+# todo rename the create functions
 @router.post("/", response=ScoreSchemaOut)
 def create_score(request, incoming_score: ScoreSchemaIn):
-    new_score = Score.objects.create(incoming_score)
+    current_highscore = Score.objects.all().order_by("-score").first()
+    score_status = check_for_highscore(current_highscore.score, incoming_score.score)
+    if not score_status:
+        return current_highscore
+    return create_score_test(request, incoming_score)
+
+
+def check_for_highscore(current_highscore, incoming_score):
+    if not current_highscore or incoming_score > current_highscore:
+        return True
+    return False
+
+
+@router.post("/score", response=ScoreSchemaOut)
+def create_score_test(request, incoming_score: ScoreSchemaIn):
+    user = User.objects.get(id=incoming_score.user)
+    new_score = Score.objects.create(score=incoming_score.score, user=user)
     new_score.save()
     return new_score
+
+
+@router.get("/", response=List[ScoreSchemaOut])
+def get_scores(request):
+    return Score.objects.all()
