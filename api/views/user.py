89b6b2abd97dbe7af84700bb2de9baf8f3dba80c
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-16 13:32:29 -0700

    added a get all users view

--- api/views/user.py
+++ api/views/user.py
@@ -1,12 +1,18 @@
+from typing import List
 from ninja import Router
 from api.models.users import User
 from api.schemas.users import UserSchemaIn, UserSchemaOut
 
 router = Router(tags=["user"])
 
-
+# DELETE THIS BEFORE PROD
 @router.post("/", response=UserSchemaOut)
-def create_user(request, new_user: UserSchemaIn):
+def create_user(request, new_user: UserSchemaIn, auth=None):
     _user = User.objects.create(username=new_user.username, password=new_user.password)
     _user.save()
     return _user
+
+
+@router.get("/", response=List[UserSchemaOut])
+def get_all_users(request):
+    return User.objects.all()
