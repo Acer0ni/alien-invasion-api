311cb8b0c2789b67d4c23f7eefe71a6deeff2686
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-17 10:22:30 -0700

    added login view

--- api/views/auth.py
+++ api/views/auth.py
@@ -1,8 +1,10 @@
 from ninja import Router
+from ninja.errors import HttpError
 from django.contrib.auth import login, logout, authenticate
 from api.schemas.auth import loginSchema
 from api.schemas.users import UserSchemaIn, UserSchemaOut
 from api.models.users import User
+
 
 router = Router(tags=["auth"])
 
@@ -14,3 +16,14 @@ def register_user(request, incoming_user: UserSchemaIn):
     new_user.save()
     login(request, new_user)
     return new_user
+
+
+@router.post("/login", response=UserSchemaOut, auth=None)
+def user_login(request, data: loginSchema):
+    logged_in_user = authenticate(
+        request, username=data.username, password=data.usernames
+    )
+    if logged_in_user is not None:
+        login(request, logged_in_user)
+        return logged_in_user
+    raise HttpError(403, "invalid credentials")
