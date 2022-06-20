171467f1dda22b05c74d001f1d32d740257b09a1
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-06-20 13:59:34 -0700

    fixed csrf

--- api/views/auth.py
+++ api/views/auth.py
@@ -1,6 +1,8 @@
+from django.http import JsonResponse
 from ninja import Router
 from ninja.errors import HttpError
 from django.contrib.auth import login, logout, authenticate
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from api.schemas.auth import loginSchema
 from api.schemas.users import UserSchemaIn, UserSchemaOut
 from api.models.users import User
@@ -11,6 +13,7 @@ router = Router(tags=["auth"])
 
 @router.post("/register", response=UserSchemaOut, auth=None)
 def register_user(request, incoming_user: UserSchemaIn):
+
     new_user = User.objects.create(**incoming_user.dict())
     new_user.set_password(incoming_user.password)
     new_user.save()
@@ -23,13 +26,19 @@ def user_login(request, data: loginSchema):
     logged_in_user = authenticate(
         request, username=data.username, password=data.password
     )
-    if logged_in_user is not None:
-        login(request, logged_in_user)
-        return logged_in_user
-    raise HttpError(403, "invalid credentials")
+    if not logged_in_user:
+        raise HttpError(403, "invalid credentials")
+    login(request, logged_in_user)
+    return logged_in_user
 
 
 @router.post("/logout")
 def user_logout(request):
     logout(request)
     return {"msg": "you have logged out succesfully"}
+
+
+@router.get("/csrf", auth=None)
+@ensure_csrf_cookie
+def get_csrf(request):
+    return JsonResponse({"msg": "it works"})
