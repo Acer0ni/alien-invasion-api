44a7965d153471a79d66af27e4d89e72ff0c063d
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-07-11 15:42:35 -0700

    Added update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+set -euo pipefail #unofficial strict mode
+git pull
+docker build -t alien_invasion_api .
+docker kill alien_invasion_api
+docker rm alien_invasion_api
+docker run --name alien_invasion_api -p 8000:8001 --env-file .env -d alien_invasion_api
