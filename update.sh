5a04c122db141e28d7b807d708413db62b3fe670
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-07-11 17:57:35 -0700

    deploy bugfix

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ git pull
 docker build -t alien_invasion_api .
 docker kill alien_invasion_api
 docker rm alien_invasion_api
-docker run --name alien_invasion_api -p 127.0.0.1:8000:8001 --env-file .env -d alien_invasion_api
+docker run --name alien_invasion_api -p 127.0.0.1:8001:8000 --env-file .env -d alien_invasion_api
